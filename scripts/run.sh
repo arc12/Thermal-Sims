7afc9e78c20922e841ecf023d05695ad22fc4b89
Adam Cooper <arc@hilltop-cottage.info>
2024-01-31 17:39:40 +0000

    remove unwanted files

--- scripts/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# for use on dev PC with manual activation of venv
-# Run this with the working direlevel "up"; i.e.ctory being one  command will be "./scripts/run"
-flask run --host=0.0.0.0 --port 8051
